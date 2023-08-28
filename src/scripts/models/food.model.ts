4831386b833ac2a3fea82aeabbaf41175a378bdd
PhamNhatHuy <huy.phamnhat@asnet.com.vn>
2023-08-28 17:06:08 +0700

    fix: update food model

--- src/scripts/models/food.model.ts
+++ src/scripts/models/food.model.ts
@@ -1,22 +1,8 @@
-export interface FoodItem {
+// Define the model of data
+export interface Food {
   id: string
   name: string
   price: number
   quantity: number
   imageUrl: string
-}
-
-/**
- * @class Food
- *
- * Define the model of the data.
- */
-export class Food implements FoodItem {
-  constructor(
-    public id: string,
-    public name: string,
-    public price: number,
-    public quantity: number,
-    public imageUrl: string
-  ) {}
 }
