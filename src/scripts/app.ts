f38d4fb5546708d817dbd185029e04f2a932e5ac
PhamNhatHuy <huy.phamnhat@asnet.com.vn>
2023-08-30 11:12:24 +0700

    fix: fix unsused app variable

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -2,5 +2,5 @@ import { FoodController } from './controllers/food.controller'
 import { FoodService } from './services.ts/food.services'
 import { FoodView } from './views/food.view'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const app = new FoodController(new FoodService(), new FoodView())
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-new
+new FoodController(new FoodService(), new FoodView())
